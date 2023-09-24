b5e1942802a767e569c0c54b2d45af7bf0d7a96b
datadiode <datadiode@users.noreply.github.com>
2023-09-24 12:15:18 +0200

    Allow unattended install through option /autoextract, placed at the end of the installer command line

--- Installer/setup.bat
+++ Installer/setup.bat
@@ -4,5 +4,7 @@ expand.exe -f:* %2 .
 echo [OpennessWebView2.dll] > OpennessWebView2.ini
 echo BrowserExecutableFolder=%~dp0%~n2 >> OpennessWebView2.ini
 :register
-regsvr32.exe OpennessWebView2.dll
+set options=
+for %%x in (%*) do if /i "%%x" == "/autoextract" set options=/s
+regsvr32.exe %options% OpennessWebView2.dll
 del %0
