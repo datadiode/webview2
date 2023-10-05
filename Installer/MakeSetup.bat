31caee81834d4b84a3ecb3914d1828b6f7cac407
datadiode <datadiode@users.noreply.github.com>
2023-10-05 12:52:34 +0200

    https://ci.appveyor.com/tools/encrypt / Change VirusTotal JSON report filename extension to .jsn to make it HTA iframe friendly / Make builds reproducible with help of https://github.com/jasonwhite/ducible

--- Installer/MakeSetup.bat
+++ Installer/MakeSetup.bat
@@ -1,8 +1,14 @@
 @echo off
 cd "%~dp0"
+set ducible.exe="..\UnityDoorstop-BSIPA\BuildUtils\ducible.exe"
+for /f "tokens=*" %%G in ('where git.exe') do (set git.exe="%%G")
+set touch.exe=%git.exe:cmd\git.exe=usr\bin\touch.exe%
 REM Read the version number from the resource file
 for /f "tokens=1,2" %%G in (..\BrowserProxy\BrowserProxy.rc) do if "%%G" == "FILEVERSION" set FILEVERSION=%%H
 echo FILEVERSION=%FILEVERSION:,=.%
+%ducible.exe% ..\BrowserProxy\Release\OpennessWebView2.dll
+for /f delims^=^"^ tokens^=2 %%G in (OpennessWebView2.ddf) do %touch.exe% -t 20230%FILEVERSION:,=% %~dp0%%G
+set FILEVERSION=%FILEVERSION:,0=,%
 REM Create the CAB
 makecab /f OpennessWebView2.ddf
 REM Create the SFX
